b23610cda407a44406fe59f1e734944d55e059d0
dustins <dustin.schoenbrun@gmail.com>
2011-07-21 00:27:56 -0400

    Initial commit of the code and documents from the ardscoreutils SVN repository formerly at sourceforge.

---
+++ ls.c
@@ -0,0 +1,16 @@
+/*
+ * ls.c - The ARDS implementation of the 'ls' utility
+ *
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char **argv)
+{
+    printf("NOT IMPLEMENTED!\n");
+
+    return EXIT_FAILURE;
+}
+
